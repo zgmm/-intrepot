4f7b3b4fe3b49837dfe0e37277cde70b58974112
邱子豪 <1213284094@qq.com>
2021-09-09 12:16:27 +0800

    第一次提交

---
+++ src/components/Load.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Load">
+        <img src="../../public/images/加载图.gif" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+    .Load{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: #e9f70a;
+        height: 100%;
+        z-index: 999;
+    }
+    .Load img{
+        width: 100%;
+        height: 100%;
+    }
+</style>
---
+++ src/components/login/Spring.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="Spring animated show">
+     <div class="spring">
+        <p><span class="iconfont icon-gantanhao"></span></p>
+        <p class="paragraph">{{paragraph}}</p>
+        <p class="confirm" @click="close()">确认</p>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Spring',
+    props: ["paragraph"],
+    methods: {
+        close() {
+           this.$emit('close')
+        },
+    },
+
+}
+</script>
+<style scoped>
+    .iconfont{
+        color: #f8cb86;
+        font-size: 1.29rem;
+    }
+    @keyframes show {
+        0% {
+            transform: scale(1.1);
+            left: 12.8%;
+        }
+        20% {
+            transform: scale(0.9);  
+            left: 12.8%;
+        }
+        40% {
+            transform: scale(1.1);
+            left: 12.8%;
+        }
+        60% {
+            transform: scale(0.9);     
+            left: 12.8%;
+        }
+        80% {
+            transform: scale(1.1);
+            left: 12.8%;
+        }
+        100% {
+            transform: scale(1);     
+            left: 12.8%;
+        }
+    }
+    
+    .Spring{
+        position: absolute;
+        width: 4.83rem;
+        height: 3.1rem;
+        top: 1.47rem;
+        transform: translateX(-50%);
+        left: 50%;
+        background: #fdfdfd;
+        animation: show 0.5s;
+    }
+    .paragraph{
+        color: #000;
+        font-size: .29rem;
+    }
+    .confirm{
+        background: #4cd964;
+        border-radius: 0 0 .14rem .14rem;
+        color: #fff;
+        font-weight: bold;
+        font-size: .34rem;
+        padding: .26rem 0;
+        margin: 0;
+    }
+</style>
