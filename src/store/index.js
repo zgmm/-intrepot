c34cc435ec7bd72a92f2f581dd84da951d26df31
wen49 <2873474992@qq.com>
2021-09-16 12:14:58 +0800

    9/16 vuex的合并

--- src/store/index.js
+++ src/store/index.js
@@ -9,29 +9,42 @@ export default new Vuex.Store({
   state: { // 相当于data
     seat: "正在定位...",
     shuliang: 0, // 定义变量
-    sum: 0,
-    name: [],
-    jiage: [],
+    sum: 0,   // 菜品总价
+    name: [],   // 菜品名字
+    jiage: [],  // 各菜品的价格
+    count:[], // 各菜品的数量
+    yeshu:0, //记录商品详情页面的页数
+    avatar:'',//头像
   },
   mutations: { // 方法区域==methods
-    add(state, index) {
+    getyeshu(state,sumber){ //记录商品详情页面的页数
+      state.yeshu=sumber
+      console.log("第"+state.yeshu+"页");
+    },
+    add(state, index) { //菜品总数量 增加
       state.shuliang++;
     },
-    jian(state, index) {
+    jian(state, index) {  //菜品总数量 减少
       state.shuliang--;
     },
-    zongjia(state, index) {
+    zongjia(state, index) { //菜品总价
       state.sum = index;
     },
-    getname(state, name) {
+    getname(state, name) {  //菜品名字
       state.name = name;
     },
-    getjiage(state, sumber) {
+    getjiage(state, sumber) { //各菜品的价格
       state.jiage = sumber;
     },
-    getsum(state, sumber) {
+    getsum(state, sumber) {  //菜品总价+配送费+餐盒费
       state.sum = sumber + 8
-    }
+    },
+    getcount(state,sumber){ //各菜品的数量
+      state.count=sumber
+    },
+    getshuliang(state,sumber){ //菜品总数量
+      state.shuliang=sumber
+    },
   },
   actions: {},
   modules: {}
